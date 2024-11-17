2627c2bc8e8770f5ea665c5a46eb55d7534cdee2
NishantPatelxx <138348371+NishantPatelxx@users.noreply.github.com>
2024-11-17 11:12:27 +0530

    #minor error will resolve leter

--- backend/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const protectedRoute = require("../middleware/protectedRoutes")
-const {sendNotification , deleteNotification} = require('../controller/notificationController')
-
-router.get("/" , protectedRoute , sendNotification)
-router.delete("/" , protectedRoute , deleteNotification)
-
-module.exports = router 
